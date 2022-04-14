1fca5d42fbe511028447d76a2bb8c2722d139910
SageblattN <s2931713@gmail.com>
2022-04-15 00:32:42 +0300

    Deleted debug output

--- AudioEngine.cpp
+++ AudioEngine.cpp
@@ -1,13 +1,14 @@
 //
 // Created by sageblatt on 13.04.2022.
 //
-
 #include "AudioEngine.h"
-#include <iostream>
-using namespace std;
+
 
 AudioEngine::AudioEngine() {
     timer = std::make_unique<sf::Clock>();
+
+    fade_flag[0] = 0;
+    fade_flag[1] = 0;
 
     for (auto i = 0; i < TRACKS_AMOUNT; i++)
         tracks[i] = std::make_unique<sf::Music>();
@@ -27,7 +28,6 @@ AudioEngine::AudioEngine() {
 
 void AudioEngine::fade(int next_song) {
     fade_flag[0] = 0;
-    cout << "1 " << next_song << endl;
     timer->restart();
     tracks[next_song]->setVolume(0);
     auto t = timer->getElapsedTime().asSeconds();
@@ -49,7 +49,6 @@ void AudioEngine::fade(int next_song) {
         tracks[current_track]->setVolume(100.f * t/FADE_LEN);
         t = timer->getElapsedTime().asSeconds();
     }
-    cout << 2 << endl;
 }
 
 void AudioEngine::run() {
@@ -65,7 +64,6 @@ void AudioEngine::run() {
 }
 
 void AudioEngine::setFadeFlag(Songs number_of_track) {
-    cout << "AMOGUS " << number_of_track << endl;
     fade_flag[0] = 1;
     fade_flag[1] = number_of_track;
 }
